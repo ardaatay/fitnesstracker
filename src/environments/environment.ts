2da1e485dff6ddda709932ebc275ed3b49e1a168
ardaatay <atay_arda@outlook.com>
2020-04-24 23:23:51 +0300

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyDJy1UdGS4CVhZbXUoPNcaokCETZbtXdoA',
-    authDomain: 'ng-fitness-tracker-e8ae5.firebaseapp.com',
-    databaseURL: 'https://ng-fitness-tracker-e8ae5.firebaseio.com',
-    projectId: 'ng-fitness-tracker-e8ae5',
-    storageBucket: 'ng-fitness-tracker-e8ae5.appspot.com',
-    messagingSenderId: '429501464515',
-    appId: '1:429501464515:web:3d87f0e7fe000527ee4776'
-  }
+  production: false
 };
 
 /*
